908e1de43f65f376bf0f5704192eeb95d024b7e3
Jaya Christina <jayachristina@gmail.com>
2023-04-05 19:33:11 +0530

    Revert changes to Category

--- src/main/java/org/globex/retail/store/catalog/model/entity/Category.java
+++ src/main/java/org/globex/retail/store/catalog/model/entity/Category.java
@@ -3,15 +3,11 @@ package org.globex.retail.store.catalog.model.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity(name = "Category")
 @Table(name = "category")
 @Cacheable
 
-@NamedQueries({
-    @NamedQuery(name = "Catalog.getCategoryList", query = "from Category")
-})
 
 public class Category extends PanacheEntityBase {
 
@@ -22,7 +18,4 @@ public class Category extends PanacheEntityBase {
     @Column(name = "category")
     public String category;
     
-    public static List<Category> getCategoryList() {
-        return find("#Catalog.getCategoryList").list();
-    }
 }
